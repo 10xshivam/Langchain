1b337b280b7f80387b586e86e2366d302051873d
Shivam <kumarshivam3788@gmail.com>
2025-09-01 02:36:23 +0530

    Enhance dynamic_prompt.py to include max_output_tokens parameter for improved output control

--- Prompts/dynamic_prompt.py
+++ Prompts/dynamic_prompt.py
@@ -12,9 +12,18 @@ prompt = template.invoke({
     'length_input': "Short (1-2 paragraphs)"
 })
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.7)
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.7,max_output_tokens=300)
 
-# Simple text invocation
 result = llm.invoke(prompt)
 print(result.content)
 
+
+
+# As we are invoking two times , so we can use chains to do this 
+# chain = template | llm
+# result = chain.invoke({
+#     'paper_input': "Attention is All You Need",
+#     'style_input': "Beginner friendly",
+#     'length_input': "Short (1-2 paragraphs)"
+# })
+# print(result.content)
