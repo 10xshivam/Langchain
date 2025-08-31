8c33fee16d4a7fcd6d6c541051d5c3493d550443
Shivam <kumarshivam3788@gmail.com>
2025-08-31 21:03:41 +0530

    Add chatmodel_gemini.py and chatmodel_hgface.py for Google Generative AI integration

---
+++ Models/ChatModels/chatmodel_hgface.py
@@ -0,0 +1,19 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="openai/gpt-oss-120b",
+    task="text-generation",
+    max_new_tokens=512,
+    do_sample=False,
+    repetition_penalty=1.03,
+    provider="auto", 
+)
+
+chat_model = ChatHuggingFace(llm=llm)
+
+result = chat_model.invoke("Who is the PM of India?")
+
+print(result)
