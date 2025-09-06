f4b4e6c7d2cc2f6e9715d2a87c4c1ce9874917c7
Shivam <kumarshivam3788@gmail.com>
2025-09-06 12:02:32 +0530

    Implement YouTube chatbot with transcript retrieval and generative AI response

---
+++ minor-projects/aiAgent.py
@@ -0,0 +1,50 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.tools import tool
+import requests
+import os
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain import hub
+from dotenv import load_dotenv
+
+load_dotenv()
+
+WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
+
+search_tool = DuckDuckGoSearchRun()
+
+@tool
+def get_weather_data(city: str) -> str:
+  """
+  This function fetches the current weather data for a given city
+  """
+  url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}'
+
+  response = requests.get(url)
+
+  return response.json()
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+# Step 2: Pull the ReAct prompt from LangChain Hub
+prompt = hub.pull("hwchase17/react")  # pulls the standard ReAct agent prompt
+
+# Step 3: Create the ReAct agent manually with the pulled prompt
+agent = create_react_agent(
+    llm=llm,
+    tools=[search_tool, get_weather_data],
+    prompt=prompt
+)
+
+# Step 4: Wrap it with AgentExecutor
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=[search_tool, get_weather_data],
+    verbose=True
+)
+
+# Step 5: Invoke
+response = agent_executor.invoke({"input": "Find the capital of Madhya Pradesh, then find it's current weather condition"})
+print(response)
+
+print(response['output'])
